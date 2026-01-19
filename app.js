5ee81e1959837ec43b4fd0057adc6d5c5305d95d
Patrick <aurelpatrickm@gmail.com>
2026-01-19 08:20:59 -0500

    feat: create gameBoard function

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.querySelector("#info");
 const startCells = ["", "", "", "", "", "", "", "", "" ];
+
+function createGameBoard() {
+    startCells.forEach((cell,index) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("square");
+        gameBoard.appendChild(cellElement);
+    })
+}
